e6522bcf8a3efcfa84334fd02fb59b98666bac7e
unknown <usuario@W0377699.2g.tj.rs>
2021-01-16 21:01:19 -0200

    tela da list de cursos

--- data/routes.py
+++ data/routes.py
@@ -3,5 +3,6 @@ def get_route(name):
     routes[''] = {'type': 'resource', 'path':'data/index.html'}
     routes['map'] = {'type': 'resource', 'path':'data/map.html'}
     routes['educationList'] = {'type': 'resource', 'path':'data/educationList.html'}
+    routes['education'] = {'type': 'resource', 'path':'data/education.html'}
     return routes[name]
     
