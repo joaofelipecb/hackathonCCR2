053c36e7fff8e32ce498acf3de9e5d4bb00f4d04
unknown <usuario@W0377699.2g.tj.rs>
2021-01-16 20:35:53 -0200

    tela do mapa

--- data/routes.py
+++ data/routes.py
@@ -1,5 +1,7 @@
 def get_route(name):
     routes = {}
     routes[''] = {'type': 'resource', 'path':'data/index.html'}
+    routes['map'] = {'type': 'resource', 'path':'data/map.html'}
+    routes['educationList'] = {'type': 'resource', 'path':'data/educationList.html'}
     return routes[name]
     
--- wsgi.py
+++ wsgi.py
@@ -2,9 +2,23 @@ import sys
 sys.path.insert(0, "C:/xampp/htdocs/hackathonCCR2")
 import control.routes
 
+def get_http_data(value):
+    parts = value.split('&')
+    parameters = {}
+    for value in parts:
+        pair = value.split('=')
+        if len(pair) == 2:
+            parameters[pair[0]] = pair[1]
+    return parameters
+
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
-    route = env['QUERY_STRING']
+    get = env['QUERY_STRING']
+    http_data = get_http_data(get)
+    if 'route' not in http_data:
+        route = ''
+    else:
+        route = http_data['route']
     result = control.routes.get_response(route)
     return [result.encode('ascii')]
     
