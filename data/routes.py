113826959921714142f1b2bf7ea7a98803ca22ae
unknown <usuario@W0377699.2g.tj.rs>
2021-01-17 15:46:59 -0200

    nova tela inicial

--- data/routes.py
+++ data/routes.py
@@ -1,6 +1,7 @@
 def get_route(name):
     routes = {}
     routes[''] = {'type': 'resource', 'path':'data/index.html'}
+    routes['geolocalization'] = {'type': 'resource', 'path':'data/geolocalization.html'}
     routes['map'] = {'type': 'resource', 'path':'data/map.html'}
     routes['educationList'] = {'type': 'resource', 'path':'data/educationList.html'}
     routes['education'] = {'type': 'resource', 'path':'data/education.html'}
