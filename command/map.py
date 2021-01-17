93418742343ecfe0e6f3319c3159091288f0449b
unknown <usuario@W0377699.2g.tj.rs>
2021-01-17 05:31:55 -0200

    plot dos dados de demanda

--- command/map.py
+++ command/map.py
@@ -11,8 +11,16 @@ def render():
     command = command+' MYSQL:"'+database['database']+',host='+database['host']+',user='+database['user']+',password='+database['password']+'" "logradouro"'
     subprocess.check_call(command, shell=True)
     logradouro = gpd.read_file(homeDir+"\\data\\logradouro.shp")
+    command = '"C:\\Program Files\\QGIS 3.16\\bin\\ogr2ogr.exe" -f "ESRI Shapefile"'
+    command = command+' "'+homeDir+'\\data\\demanda.shp"'
+    command = command+' MYSQL:"'+database['database']+',host='+database['host']+',user='+database['user']+',password='+database['password']+'" "demanda"'
+    command = command+' -where "distancia > 10"'
+    subprocess.check_call(command, shell=True)
+    demanda = gpd.read_file(homeDir+"\\data\\demanda.shp")
     base = logradouro.plot(color='black', edgecolor='black', linewidth=0.1)
     base.set_aspect(1)
+    pontos = demanda.plot(ax=base, color='red', edgecolor='black', markersize=0.5, linewidth=0)
+    pontos.set_aspect(1)
     base.set_axis_off()
     chart = base.get_figure()
     filepath = homeDir + '/data/'+ 'mapa.jpg'
